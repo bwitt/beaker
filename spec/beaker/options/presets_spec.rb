2fd5bf495cd16cdcad51094d17e4599dc8ba0b31
Brian Witt <brianwitt@gmail.com>
2026-01-04 10:55:45 -0800

    respect no_color

--- spec/beaker/options/presets_spec.rb
+++ spec/beaker/options/presets_spec.rb
@@ -23,6 +23,56 @@ module Beaker
         expect(env[:run_in_parallel]).to eq(%w[install configure])
       end
 
+      describe "color environment variables" do
+        after do
+          ENV.delete('NO_COLOR')
+          ENV.delete('BEAKER_color')
+        end
+
+        it "disables color when NO_COLOR is set to any non-empty value" do
+          ENV['NO_COLOR'] = '1'
+          env = presets.env_vars
+          expect(env[:color]).to be false
+        end
+
+        it "does not set color when NO_COLOR is set to an empty string" do
+          ENV['NO_COLOR'] = ''
+          env = presets.env_vars
+          expect(env).not_to have_key(:color)
+        end
+
+        it "disables color when BEAKER_color is set to 'no'" do
+          ENV['BEAKER_color'] = 'no'
+          env = presets.env_vars
+          expect(env[:color]).to be false
+        end
+
+        it "disables color when BEAKER_color is set to 'false'" do
+          ENV['BEAKER_color'] = 'false'
+          env = presets.env_vars
+          expect(env[:color]).to be false
+        end
+
+        it "enables color when BEAKER_color is set to 'yes'" do
+          ENV['BEAKER_color'] = 'yes'
+          env = presets.env_vars
+          expect(env[:color]).to be true
+        end
+
+        it "enables color when BEAKER_color is set to 'true'" do
+          ENV['BEAKER_color'] = 'true'
+          env = presets.env_vars
+          expect(env[:color]).to be true
+        end
+
+        it "prioritizes NO_COLOR over BEAKER_color" do
+          ENV['NO_COLOR'] = '1'
+          ENV['BEAKER_color'] = 'yes'
+          env = presets.env_vars
+          expect(env[:color]).to be false
+        end
+      end
+
       it "removes all empty/nil entries in env_vars" do
         expect(presets.env_vars.has_value?(nil)).to be === false
         expect(presets.env_vars.has_value?({})).to be === false
